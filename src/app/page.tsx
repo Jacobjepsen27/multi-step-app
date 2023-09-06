6d0e62fada198f003eb61a21f231c66b120d74ce
Jacob Worck Jepsen <jacob@worckjepsen.dk>
2023-09-06 10:35:09 +0200

    Add: initialise xstate from session storage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Onboarding from "@/modules/onboarding";
 import { flex } from "../../styled-system/patterns";
-import StepMachineProvider from "@/statemachine/OnboardingMachineProvider";
+import OnboardingMachineProvider from "@/statemachine/OnboardingMachineProvider";
 
 export default function Home() {
   return (
     <div className={flex({ direction: "column", bg: "magnolia", height: "100%" })}>
-      <StepMachineProvider>
+      <OnboardingMachineProvider>
         <Onboarding />
-      </StepMachineProvider>
+      </OnboardingMachineProvider>
     </div >
   )
 }
--- src/statemachine/OnboardingMachineProvider.tsx
+++ src/statemachine/OnboardingMachineProvider.tsx
@@ -1,15 +1,29 @@
 "use client"
 import { useInterpret } from "@xstate/react";
-import React from "react";
+import React, { useEffect } from "react";
 import onboardingMachine, { OnboardingMachineInterpreter } from "./onboardingMachine";
 
 
 type OnboardingContextValue = { service: OnboardingMachineInterpreter };
-
 export const OnboardingMachineReactContext = React.createContext({} as OnboardingContextValue);
 
+const SESSION_STORAGE_KEY = "onboarding-state";
+
+const retrieveSavedState = () => {
+    const sessionStorageState = sessionStorage.getItem(SESSION_STORAGE_KEY);
+    return sessionStorageState ? JSON.parse(sessionStorageState) : onboardingMachine.initialState;
+}
+
 const OnboardingMachineProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const service = useInterpret(onboardingMachine);
+    const service = useInterpret(onboardingMachine).start(retrieveSavedState());
+
+    // Persist state changes to allow refresh of page.
+    useEffect(() => {
+        const subscription = service.subscribe((state) => {
+            sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(state));
+        });
+        return () => subscription.unsubscribe();
+    }, [service]);
 
     return <OnboardingMachineReactContext.Provider value={{ service }}>
         {children}
